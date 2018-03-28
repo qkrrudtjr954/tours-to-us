6f96bf04624731901a301df9ae714ba0f9057fcf
HyenSun <dkfkehs02@naver.com>
2018-03-28 14:04:28 +0900

    afterview로 넘어감 게시판 작성 진행중

--- src/www/tours2us/com/controller/CommuController.java
+++ src/www/tours2us/com/controller/CommuController.java
@@ -3,12 +3,30 @@ package www.tours2us.com.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 
 @Controller
 public class CommuController {
 private static final Logger logger = LoggerFactory.getLogger(MainController.class);	
 	
+@RequestMapping(value="afterBbs.do", 
+method= {RequestMethod.GET, RequestMethod.POST})
+public String afterBbs(Model model) throws Exception{
 	
+	logger.info("CommuController >>>> afterbbslist");
+	
+	return "afterBbs.tiles";
+
+	
+	
+	
+}
+
+
 	
 	
 

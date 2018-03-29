17d70c66b643c7be96b788ca74ee507d7779981a
Iseul <rladltmf813@naver.com>
2018-03-29 13:14:25 +0900

    마이페이지 리스트

--- src/www/tours2us/com/model/TravelerDto.java
+++ src/www/tours2us/com/model/TravelerDto.java
@@ -11,6 +11,10 @@ public class TravelerDto implements Serializable {
 	private String reg_date;
 	private String last_updated;
 	private int author;
+	private String profile;
+	private String like1;
+	private String like2;
+	private String like3;
 	
 	public TravelerDto() {
 		// TODO Auto-generated constructor stub
@@ -26,6 +30,23 @@ public class TravelerDto implements Serializable {
 		this.reg_date = reg_date;
 		this.last_updated = last_updated;
 		this.author = author;
+	}
+	
+
+	public TravelerDto(int seq, String email, String password, String name, String reg_date, String last_updated,
+			int author, String profile, String like1, String like2, String like3) {
+		super();
+		this.seq = seq;
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.reg_date = reg_date;
+		this.last_updated = last_updated;
+		this.author = author;
+		this.profile = profile;
+		this.like1 = like1;
+		this.like2 = like2;
+		this.like3 = like3;
 	}
 
 	public int getSeq() {
@@ -84,13 +105,44 @@ public class TravelerDto implements Serializable {
 		this.author = author;
 	}
 
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public String getLike1() {
+		return like1;
+	}
+
+	public void setLike1(String like1) {
+		this.like1 = like1;
+	}
+
+	public String getLike2() {
+		return like2;
+	}
+
+	public void setLike2(String like2) {
+		this.like2 = like2;
+	}
+
+	public String getLike3() {
+		return like3;
+	}
+
+	public void setLike3(String like3) {
+		this.like3 = like3;
+	}
+
 	@Override
 	public String toString() {
 		return "TravelerDto [seq=" + seq + ", email=" + email + ", password=" + password + ", name=" + name
-				+ ", reg_date=" + reg_date + ", last_updated=" + last_updated + ", author=" + author + "]";
+				+ ", reg_date=" + reg_date + ", last_updated=" + last_updated + ", author=" + author + ", profile="
+				+ profile + ", like1=" + like1 + ", like2=" + like2 + ", like3=" + like3 + "]";
 	}
-	
-	
-	
+
 	
 }
